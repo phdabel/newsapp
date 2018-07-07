9149764054252a9c238204b2dad6ec976aaf5c1f
Unknown <theolm.mota@gmail.com>
2018-07-07 16:46:06 -0300

    pagination fix

--- app/src/main/java/abelcorreadias/com/br/newsapp/MainActivity.java
+++ app/src/main/java/abelcorreadias/com/br/newsapp/MainActivity.java
@@ -7,13 +7,14 @@ import android.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +23,6 @@ import abelcorreadias.com.br.newsapp.adapters.NewsRecyclerAdapter;
 import abelcorreadias.com.br.newsapp.listeners.EndlessRecyclerViewScrollListener;
 import abelcorreadias.com.br.newsapp.loaders.NewsLoader;
 import abelcorreadias.com.br.newsapp.models.NewsItem;
-import abelcorreadias.com.br.newsapp.network.Connection;
 
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsItem>> {
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private EndlessRecyclerViewScrollListener scrollListener;
 
-    private RecyclerView.LayoutManager layoutManager;
+    private LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -49,28 +49,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
-
         progressBar = (ProgressBar) findViewById(R.id.loading_spinner);
         emptyStateTextView = (TextView) findViewById(R.id.empty_view);
+
         emptyStateTextView.setText(R.string.no_news_found);
 
-        adapter = new NewsRecyclerAdapter(new ArrayList<NewsItem>());
-        recyclerView.setAdapter(adapter);
+        setupRecyclerview();
 
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if(networkInfo != null && networkInfo.isConnected()){
-            LoaderManager loaderManager = getLoaderManager();
-            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
-        }else{
-            progressBar.setVisibility(View.GONE);
-            recyclerView.setVisibility(View.GONE);
-            emptyStateTextView.setText(R.string.no_internet_connection);
-        }
+        setupConnectivity();
+
     }
 
     @Override
@@ -80,11 +67,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(final Loader<List<NewsItem>> loader, List<NewsItem> data) {
-
         progressBar.setVisibility(View.GONE);
         emptyStateTextView.setText(R.string.no_news_found);
-        if(data != null && !data.isEmpty()){
-            adapter = new NewsRecyclerAdapter(data);
+        if (data != null && !data.isEmpty()) {
+            adapter.addAll(data);
             adapter.setOnItemClickListener(new NewsRecyclerAdapter.OnItemClickListener() {
 
                 @Override
@@ -96,26 +82,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
             });
         }
-        recyclerView.setAdapter(adapter);
-        scrollListener = new EndlessRecyclerViewScrollListener((LinearLayoutManager) layoutManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                /*
-                final int curSize = adapter.getItemCount();
-                adapter.addAll(moreNews);
-                //adapter.notifyItemRangeInserted(curSize, adapter.getItemCount()-1);
-                view.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        adapter.notifyItemRangeInserted(curSize, adapter.getItemCount()-1);
-                    }
-                });
-                */
-                getLoaderManager().restartLoader(NEWS_LOADER_ID, null, (MainActivity)view.getContext());
-            }
-        };
-        recyclerView.addOnScrollListener(this.scrollListener);
-        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -123,5 +89,39 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         adapter = new NewsRecyclerAdapter(new ArrayList<NewsItem>());
         recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+    }
+
+    private void setupRecyclerview() {
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        adapter = new NewsRecyclerAdapter(new ArrayList<NewsItem>());
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Toast.makeText(MainActivity.this, "onLoadMore " + String.valueOf(page), Toast.LENGTH_SHORT).show();
+                //TODO: Trocar esse método por uma chamada do loader page+1... com esse reinicia sempre o loader e fica chamando a page 1 sempre.
+                getLoaderManager().restartLoader(NEWS_LOADER_ID, null, (MainActivity) view.getContext());
+            }
+        };
+
+        recyclerView.addOnScrollListener(this.scrollListener);
+
+    }
+
+    private void setupConnectivity() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
     }
 }
